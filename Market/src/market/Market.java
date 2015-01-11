b6fea39e958db7184ed54fb8ace4831e0fd9668e
Justin <yurkanin321@gmail.com>
2015-01-11 15:07:06 -0500

    initialization

---
+++ Market/src/market/Market.java
@@ -0,0 +1,14 @@
+package market;
+
+import org.uncommons.watchmaker.*;
+public class Market {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
